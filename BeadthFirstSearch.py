eb8d233728a0b29975b76705944d861c6ce7a269
singularity014 <49594316+singularity014@users.noreply.github.com>
2020-05-12 22:56:49 +0200

    Update BeadthFirstSearch.py
    updating python formatting

--- BeadthFirstSearch.py
+++ BeadthFirstSearch.py
@@ -2,40 +2,40 @@
 class Node(object):
 
 	def __init__(self, name):
-		self.name = name;
-		self.adjacencyList = [];
-		self.visited = False;
-		self.predecessor = None;
+		self.name = name
+		self.adjacencyList = []
+		self.visited = False
+		self.predecessor = None
 		
 class BreadthFirstSearch(object):
 
 	def bfs(self, startNode):
 	
-		queue = [];
-		queue.append(startNode);
-		startNode.visited = True;
+		queue = []
+		queue.append(startNode)
+		startNode.visited = True
 		
 		# BFS -> queue      DFS --> stack BUT usually we implement it with recursion !!!
 		while queue:
 		
-			actualNode = queue.pop(0);
-			print("%s " % actualNode.name);
+			actualNode = queue.pop(0)
+			print("%s " % actualNode.name)
 			
 			for n in actualNode.adjacencyList:
 				if not n.visited:
-					n.visited = True;
-					queue.append(n);
+					n.visited = True
+					queue.append(n)
 					
-node1 = Node("A");
-node2 = Node("B");
-node3 = Node("C");
-node4 = Node("D");
-node5 = Node("E");
+node1 = Node("A")
+node2 = Node("B")
+node3 = Node("C")
+node4 = Node("D")
+node5 = Node("E")
 
-node1.adjacencyList.append(node2);
-node1.adjacencyList.append(node3);
-node2.adjacencyList.append(node4);
-node4.adjacencyList.append(node5);
+node1.adjacencyList.append(node2)
+node1.adjacencyList.append(node3)
+node2.adjacencyList.append(node4)
+node4.adjacencyList.append(node5)
 
-bfs = BreadthFirstSearch();
-bfs.bfs(node1);
+bfs = BreadthFirstSearch()
+bfs.bfs(node1)
